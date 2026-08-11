64d43288bf9c92b4f005f79a12f1c3b33d162ab1
localhost433 <tc4119@nyu.edu>
2026-08-10 20:39:32 -0400

    fix

--- Tutor.java
+++ Tutor.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.io.Serializable;
 public class Tutor implements Serializable{
+    private static final long serialVersionUID = 1L;
+
     private ArrayList<Opening> AvailableOpenings = new ArrayList<>();
     private String name;
     private String password;
